7a10eb4d48b1e3951a90e81d683c5ec35367f61a
Steven Jin <vlsijin@gmail.com>
2018-01-10 23:08:46 -0800

    Update CountNumberOfTreesInBST.java
    I think you might have forgotten T[2] case. If you add T[i] = 0; in line 30, the code will okay.

--- src/com/interview/dynamic/CountNumberOfTreesInBST.java
+++ src/com/interview/dynamic/CountNumberOfTreesInBST.java
@@ -27,6 +27,7 @@ public class CountNumberOfTreesInBST {
         T[0] = 1;
         T[1] = 1;
         for(int i=2; i <= n; i++){
+            T[i] = 0;
             for(int j=0; j <i; j++){
                 T[i] += T[j]*T[i-j-1];
             }
